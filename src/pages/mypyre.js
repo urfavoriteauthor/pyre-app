9755b7e2b4ac1f11a4de7944e8824faa7275cf57
Nygel Porterfield <nygel.porterfield@gmail.com>
2020-01-09 23:21:10 -0500

    h

--- src/pages/mypyre.js
+++ src/pages/mypyre.js
@@ -5,7 +5,7 @@ class Mypyre extends React.Component{
     render(){
         return(
 <div>
-    
+    <h1>My Pyre</h1>
 </div>
         )
     }
--- src/pages/network.js
+++ src/pages/network.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 class Network extends React.Component{
     render(){
-        return
+        return(
+            <div>
+                
+            </div>
+        )
     }
 }
 
